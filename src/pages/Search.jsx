54c45d0e6d915b384abc30a88f588b5fbb40d59f
joao <joao.dev94@gmail.com>
2022-09-13 05:10:52 -0300

    Requisito 05

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,42 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  state = {
+    inputValue: '',
+    releaseBtn: true,
+  };
+
+  handleChange = ({ target }) => {
+    const { inputValue } = this.state;
+    this.setState({
+      [target.name]: target.value,
+    }, () => {
+      if (inputValue.length >= 1) {
+        this.setState({
+          releaseBtn: false,
+        });
+      }
+    });
+  };
+
   render() {
+    const { releaseBtn } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
         <h1>Search</h1>
+        <input
+          type="text"
+          name="inputValue"
+          data-testid="search-artist-input"
+          onChange={ this.handleChange }
+        />
+        <input
+          type="button"
+          value="Pesquisar"
+          data-testid="search-artist-button"
+          disabled={ releaseBtn }
+        />
       </div>
     );
   }
