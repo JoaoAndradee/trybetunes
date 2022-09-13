bfdaf575ecfbcabe0dd3a0e41c16c11067501cd2
joao <joao.dev94@gmail.com>
2022-09-13 04:50:33 -0300

    Requisito 04

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import LoadingText from './LoadingText';
 
 class Header extends React.Component {
   state = {
-    // name: '',
     loading: true,
   };
 
@@ -23,6 +23,9 @@ class Header extends React.Component {
       <header data-testid="header-component">
         <p data-testid="header-user-name">{`Olá ${name}`}</p>
         { loading && <LoadingText /> }
+        <Link to="/search" data-testid="link-to-search">Pesquisa</Link>
+        <Link to="/favorites" data-testid="link-to-favorites">Músicas Favoritas</Link>
+        <Link to="/profile" data-testid="link-to-profile">Meu Perfil</Link>
       </header>
     );
   }
